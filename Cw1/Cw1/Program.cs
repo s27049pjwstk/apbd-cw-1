b5560989c6cf28e9e492faa18e83f3d30705e83b
s27049 <s27049@pjwstk.edu.pl>
2024-03-11 13:09:36 +0100

    Pierwszy Commit

---
+++ Cw1/Cw1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //1. Primitive
+        int g = 10;
+        int h2 = g;
+        //string = String
+        //Nullable<int> = int?
+        Console.WriteLine("Test Commit");
+        
+
+        //2. Complex
+        //Student std;
+        Console.WriteLine("Hello, World!");
+    }
+}
