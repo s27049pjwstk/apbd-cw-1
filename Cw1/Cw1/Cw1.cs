a9877e0a025eca316c8bd62c41192293c7fa0813
s27049 <s27049@pjwstk.edu.pl>
2024-03-15 16:33:18 +0100

    Zad3 Implementacja liczenia średniej

--- Cw1/Cw1/Cw1.cs
+++ Cw1/Cw1/Cw1.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
+namespace Cw1;
+
+public class Cw1 {
+    public static void Main(string[] args) {
         //1. Primitive
         int g = 10;
         int h2 = g;
@@ -12,16 +12,24 @@ public class Program
         Console.WriteLine("Test Commit");
         Console.WriteLine("Commit v2");
         Console.WriteLine("Commit w IDE");
-        
+
 
         //2. Complex
         //Student std;
         Console.WriteLine("Hello, World!");
     }
 
-    public static void Calc(int a, int b)
-    {
+    public static void Calc(int a, int b) {
         int c = a + b;
         Console.WriteLine("hahaha");
+    }
+
+    public static double Avg(int[] input) {
+        int sum = 0;
+        foreach (int e in input) {
+            sum += e;
+        }
+
+        return (double)sum / input.Length;
     }
 }
